bdc8345d4fceca483502c142f12e006c5fa0d208
Luis Daniel Cera Barros <ceraluis4@gmail.com>
2025-06-16 19:51:49 -0500

    feat: actualizacion

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// let message
+
+// message='soy Luis Cera'
+// message='soy un coder de riwi'
+
+// const tipoDeSangre = 'A+'
+
+// alert(message)
+
+// console.log(message)
+// console.error(message)
+// console.warn(message)
+
+let myWeight
+let myHeight
+
+myWeight=prompt(message='please, write your weight')
+myHeight=prompt(message='please, write your height')
+
+imc = myWeight / (myHeight * myHeight)
+
+
+alert('your IMC is '+ imc)
